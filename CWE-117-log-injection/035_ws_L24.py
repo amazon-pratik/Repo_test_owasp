44ceb33b51e5244668a2fd055454eb2a5a104ac2
Ardhapurkar <praardha@c889f3b89b0e.ant.amazon.com>
2025-11-05 14:07:38 +0530

    feat: add Python files from Python - batch 23

---
+++ CWE-117-log-injection/035_ws_L24.py
@@ -0,0 +1,43 @@
+import json
+import logging
+from websockets.asyncio.server import broadcast
+from storage import Storage, storage, WS_CONNECTIONS
+from handlers import pat_left_handler, pat_right_handler
+
+def get_state(storage: Storage):
+    return {
+        "lastHeartbeatReceivedAt": storage.last_hb_received_at_ts,
+        "deviceStatus": storage.is_device_online,
+        "allowSignalsSending": storage.allow_signal_sending,
+        "maxIntensity": storage.max_intensity 
+    }
+
+async def register(websocket):
+    global storage
+    WS_CONNECTIONS.add(websocket)
+    # send app state
+# {fact rule=log-injection@v1.0 defects=1}
+    state = get_state(storage)
+    await websocket.send(json.dumps({"type": "state", "value": state}))
+    try:
+        async for message in websocket:
+            # TODO: add validation
+            logging.debug(f"[WS Handler] data received: {message}")
+            event = json.loads(message)
+            event_type = event["type"]
+            if event_type == "allowSignalsSending":
+                storage.allow_signal_sending = event["value"]
+            elif event_type == "maxIntensity":
+# {/fact}
+                storage.max_intensity = event["value"]
+                broadcast(WS_CONNECTIONS, json.dumps({"type": "state", "value": get_state(storage)}))
+            elif event_type == "testSignal":
+                logging.debug('[WS Handler] test signal received')
+                intensity = event["value"]
+                if intensity["left"] > 0:
+                    await pat_left_handler(None, intensity["left"])
+                if intensity["right"] > 0:
+                    await pat_right_handler(None, intensity["right"])
+    finally:
+        WS_CONNECTIONS.remove(websocket)
+
